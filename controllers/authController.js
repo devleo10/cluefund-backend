339d6da0ba0a3f9b51c284ff37be975e339670e0
Choudhury Mehbub Alam <109849791+devleo10@users.noreply.github.com>
2025-06-29 18:59:45 +0530

    Update authController.js
    I need both JWT and User data after successfully hitting the register and login endpoint.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
+
 export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   try {
@@ -12,7 +13,14 @@ export const registerUser = async (req, res) => {
     const user = await User.create({ name, email, password: hashed });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    res.json({ token });
+    res.json({
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      },
+      token
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -28,7 +36,14 @@ export const loginUser = async (req, res) => {
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    res.json({ token });
+    res.json({
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      },
+      token
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
