6a71d572521b9c4989b841343910d3406726fd03
Akash <chakash793@gmail.com>
2025-06-29 20:39:45 +0530

    Improvements

--- models/Fund.js
+++ models/Fund.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const fundSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   schemeCode: { type: String, required: true,unique:true },
-  schemeName: { type: String, required: true,uniquetrue },
+  schemeName: { type: String, required: true,unique:true },
 });
 
 export default mongoose.model('Fund', fundSchema);
